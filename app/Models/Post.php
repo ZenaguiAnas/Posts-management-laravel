2309c8cb0ed406d0b364c67239a59d8da65d5023
Anas ZENAGUI <zenaguianas20@gmail.com>
2023-03-07 23:56:44 +0100

    Adding elequent relation one to many between Post and Comment Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,8 @@ class Post extends Model
 {
     // use HasFactory;
     protected $fillable = ['title', 'content', 'slug', 'active'];
+
+    public function comments(){
+        return $this->hasMany('app\Models\Comment');
+    }
 }
