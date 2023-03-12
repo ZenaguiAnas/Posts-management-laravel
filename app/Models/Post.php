bc6ddf40d7e89221a056e59b68199d1c3ff9cef2
Anas ZENAGUI <zenaguianas20@gmail.com>
2023-03-12 20:15:04 +0100

    Manipulating the Model Factory for Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    // use HasFactory;
+    use HasFactory;
     protected $fillable = ['title', 'content', 'slug', 'active'];
 
     public function comments(){
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Faker\Factory as FakerFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+     protected $model = Post::class;
+
+
+    public function definition(): array
+    {
+        $title = fake()->realText(67);
+        
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title, '-'),
+            'content' => fake()->text,
+            'active' => fake()->boolean,
+        ];
+    }
+}
